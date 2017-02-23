7a5a676024359ecc73c5f92d2509ecf9031615a8
Katy McCann <katy.alice.mccann@gmail.com>
2017-02-23 14:42:29 +0000

    Create method prevent_quality_from_going_below_zero, and include it in update quality methods

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -7,7 +7,7 @@ class GildedRose
   end
 
   def update_quality()
-    sort_item_by_sell_by_date
+    sort_items_by_sell_by_date
     update_quality_within_sell_by_date
     update_quality_past_sell_by_date
   end
@@ -24,13 +24,19 @@ class GildedRose
 
   def update_quality_within_sell_by_date
     @items_within_their_sell_by_date.each { |item| (item.sell_in -= 1) && (item.quality -= 1)}
+    prevent_quality_from_going_below_zero(@items_within_their_sell_by_date)
   end
 
   def update_quality_past_sell_by_date
-      @items_past_their_sell_by_date.each { |item|(item.sell_in -= 1) && (item.quality -= 2)}
+    @items_past_their_sell_by_date.each { |item|(item.sell_in -= 1) && (item.quality -= 2)}
+    prevent_quality_from_going_below_zero(@items_past_their_sell_by_date)
+  end
+
+  def prevent_quality_from_going_below_zero(items)
+    @items = items
+    @items.map{|item| item.quality = 0 if item.quality <= 0 && item.sell_in <= 0}
   end
 end
-
 
 class Item
   attr_accessor :name, :sell_in, :quality
